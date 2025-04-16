7a64fd9ec95b757d1d12b7dba94da3611f663565
Lecturer Sam <lecturersamdotcom@gmail.com>
2025-04-16 17:03:51 +0000

    Add Tailwind Styling and configs

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4361ee",
+        secondary: "#3f37c9",
+        accent: "#4895ef"
+      },
+    },
+  },
+  plugins: [],
+}
